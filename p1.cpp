7eba2723aa570e639b52ba5502218d0c916d9caf
Faiqfarmz <faiqfarmz@gmail.com>
2025-03-10 11:32:08 +0700

    first commit

---
+++ p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float luas, panjang, lebar; //var global
+
+void inputdata(){
+    cout << "masukkan panjangnya:";
+    cin >> panjang;
+
+    cout << "masukkan lebarnya";
+    cin >> lebar;
+}
+
+void prosedurluas(){
+    luas = panjang * lebar; 
+}
+
+
+void prosedurluas(){
+    cout << "luas persegi panjang:" << luas << endl;
+}
+
+
+
+int main()
+{
+    inputdata();
+    prosedurluas();
+    tampilkanluas();
+}
---
+++ p3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+float hitungRerata(int x, int y){
+    return (x+y)/2;
+}
+
+string statusKelulusan(float a){
+    if (a >= 75)
+        return "lulus";
+    else
+        return "gagal";
+}
+
+int main(){//begin
+    // numeric nilai1, nilai2, rerata;
+    int nilai1, nilai2;
+    // character statur;
+    string statur;
+
+    // display "masukkan nilai pertama: "
+    cout << "masukkan nilai pertama: ";
+    // accept nilai1
+    cin >> nilai1;
+    // display "masukkan nilai kedua: "
+    cout << "masukkan nilai kedua: ";
+    // accept nilai2
+    cin >> nilai2;
+
+    // rerata = (nilai1+nilai2)/2;
+
+    // if (rerata >= 75)
+    //      status = "lulus";
+    // else
+    //      status = "gagal";
+
+    // display "status kelulusan =" + status
+    cout << "status kelulusan: " << statusKelulusan(hitungRerata(nilai1, nilai2));
+
+}//end
